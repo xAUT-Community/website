c8b507cf78c2ad057485be29c341c790115bfaf4
AustrianNoah <marcel.w@friendlyhost.email>
2025-09-26 11:13:24 +0200

    changed something in the next.config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    devIndicators: false,
+
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 };
 
 export default nextConfig;
